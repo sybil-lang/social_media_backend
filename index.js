e31522ce05e3e5ea73df091bd59a0578bea1f0bd
sybil-lang <78490279+sybil-lang@users.noreply.github.com>
2024-07-22 16:33:49 +0530

    addinng env for port

--- index.js
+++ index.js
@@ -10,41 +10,34 @@ const postRoute = require("./routes/posts");
 
 dotenv.config();
 
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect("mongodb+srv://charuprabha228:6LoUGtIosjdr1Z1r@cluster0.lpkicec.mongodb.net/socialmedia_builder?retryWrites=true&w=majority&appName=Cluster0", {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     });
-//     console.log("Connected to MongoDB");
-//   } catch (error) {
-//     console.error("Failed to connect to MongoDB", error);
-//     process.exit(1);
-//   }
-// };
-
-// connectDB();
-const connectDB = async (req, res) => {
+const connectDB = async () => {
   try {
-      await mongoose.connect(process.env.MONGO_URL).then(() => {
-          console.log("Connected")
-      })
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("Connected");
   } catch (error) {
-      res.status(400).json({ message: "Not Connected" });
+    console.error("Connection error", error);
   }
-}
+};
 
-connectDB()
+connectDB();
 
-//middleware
-app.use(express.json());
-app.use(helmet());
-app.use(morgan("common"));
+// middleware
+// Middleware to parse incoming JSON requests
+app.use(express.json()); 
+
+// Middleware to enhance API security by setting various HTTP headers
+app.use(helmet()); 
+
+// Middleware for logging HTTP requests and responses
+app.use(morgan("common")); 
+
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 
-app.listen(8800, () => {
-  console.log("Backend server is running!");
+const PORT = process.env.PORT || 8800;
+
+app.listen(PORT, () => {
+  console.log(`Backend server is running on port ${PORT}!`);
 });
